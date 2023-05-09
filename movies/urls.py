0f4147a012bc56a1090e90b684b5d8c00fd7fa51
Macheyeki Isack <misack@users.noreply.github.com>
2023-05-09 16:45:13 +0300

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.view_movies, name="view_movies" ),
-    path('add_movie',views.add_movie, name="add_movie")
-    
-    # path('delete_mention/<int:myid>/',views.delete_mention, name = 'delete_mention'),
-    # path('edit_mention/<int:myid>/',views.edit_mention, name = 'edit_mention'),
-    # path('update_mention/<int:myid>/',views.update_mention, name = 'update_mention')
+    path('add_movie',views.add_movie, name="add_movie"),
+    path('view_movie/<int:pk>',views.view_movie, name="view_movie"),
+    path('edit_movie/<int:pk>',views.edit_movie, name="edit_movie"),
+    path('update_movie/<int:myid>/',views.update_movie, name = 'update_movie'),
+    path('delete_movie/<int:myid>/',views.delete_movie, name = 'delete_movie')
 ]
