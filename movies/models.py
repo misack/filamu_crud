040ee9ae841499565899f6109994c2a0f7728771
Macheyeki Isack <misack@users.noreply.github.com>
2023-05-09 17:19:17 +0300

    Update models.py

--- movies/models.py
+++ movies/models.py
@@ -13,4 +13,7 @@ class Movie(models.Model):
     cover = models.ImageField(upload_to="media")
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
     
