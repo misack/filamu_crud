a848428feb8952ff05ed9d7d69d61c07cbb91469
Macheyeki Isack <misack@users.noreply.github.com>
2023-08-07 12:07:50 +0300

    static files

--- filamu/settings.py
+++ filamu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t29j%g=@$bk-@r^yrx8ke*r=4u%c6*+5)du_cr(@y^+aq$2c^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #new 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
